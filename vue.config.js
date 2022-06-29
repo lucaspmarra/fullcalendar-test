ef6f01a9aa88591a5dd9d4fe311d5d6218485ef2
Lucas Paulino Marra <lucas.marra.terceirizado@tre-go.jus.br>
2022-06-29 16:49:04 -0300

    update on vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 //vue.config.js
 module.exports = {
-    publicPath: '/',
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/fullcalendar-test/' :
+        '/',
     chainWebpack: config => {
         config
             .plugin('html')
