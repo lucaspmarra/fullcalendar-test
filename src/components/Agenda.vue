715458d5b750b436f7f7cc703920645b50e8d8c6
Lucas Paulino Marra <lucas.marra.terceirizado@tre-go.jus.br>
2022-06-30 07:45:50 -0300

    create prompt to get title of event

--- src/components/Agenda.vue
+++ src/components/Agenda.vue
@@ -3,7 +3,11 @@
     <h3 class="text-center">Events Calendar</h3>
     <div class="card mx-auto">
       <div class="card-body shadow">
-        <Fullcalendar :options="calendarPlugins" :header="headerToolbar">
+        <Fullcalendar
+          :options="config"
+          :header="headerToolbar"
+          :events="events"
+        >
           <template #eventContent="{ timeText, event }">
             <b>{{ timeText }}</b>
             <i>{{ event.title }}</i>
@@ -22,46 +26,76 @@ import InteractionPlugin from "@fullcalendar/interaction";
 import ListPlugin from "@fullcalendar/list";
 
 // import EventModal from "./EventModal.vue";
-import { mapGetters, mapState } from "vuex";
+import { mapGetters, mapState, mapActions } from "vuex";
 
 export default {
   name: "Agenda",
   data() {
     return {
-      calendarPlugins: {
-        plugins: [DayGridPlugin, TimeGridPlugin, InteractionPlugin, ListPlugin],
-        initialView: "dayGridMonth",
-        headerToolbar: {
-          start: "dayGridMonth timeGridWeek timeGridDay",
-          center: "title",
-          end: "prev today next",
-        },
-        selectable: true,
-        selectMirror: true,
-        editable: true,
-        dayMaxEvents: true,
-        event: this.events,
-        dateClick: this.handleDateClick,
-        // eventClick: this.handleDateClick,
+      configOptions() {
+        return {
+          plugins: [
+            DayGridPlugin,
+            TimeGridPlugin,
+            InteractionPlugin,
+            ListPlugin,
+          ],
+          initialView: "dayGridMonth",
+          headerToolbar: {
+            start: "dayGridMonth timeGridWeek timeGridDay",
+            center: "title",
+            end: "prev today next",
+          },
+          selectable: true,
+          selectMirror: true,
+          editable: true,
+          dayMaxEvents: true,
+          event: this.events,
+        };
+      },
+      eventHandlers() {
+        return {
+          dateClick: this.handleDateClick,
+          eventClick: this.onEventClick,
+          eventDrop: this.onEventDrop,
+          eventResize: this.onEventDrop,
+          select: this.onDateSelect,
+        };
       },
     };
   },
   components: { Fullcalendar },
   computed: {
     ...mapGetters(["events"]),
-    ...mapState(["events"]),
+    config() {
+      return {
+        ...this.configOptions(),
+        ...this.eventHandlers(),
+      };
+    },
   },
   methods: {
+    ...mapActions([
+      "createEvent",
+      "updateEvent",
+      "deleteEvent",
+      "setweekendsVisible",
+    ]),
+
     handleDateClick(arg) {
       /* Get click event data */
       console.log(arg);
-      /* Get event data in store */
+      /* Get event data in store  */
       console.log(this.$store.state.events);
-
+      const title = prompt("Please enter a title for your event");
+      if (!title) {
+        return undefined;
+      }
       this.$store.commit("addEvent", {
         id: new Date().getTime(),
-        title: "Some event",
+        title: title,
         start: arg.dateStr,
+        end: arg.end,
         allDay: arg.allDay,
       });
     },
